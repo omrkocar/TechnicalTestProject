fc4d894946e37a4822137cf2c7b621b8265fc171
omrkocar <kocartahir@gmail.com>
2025-06-05 22:00:54 -0400

    Created buttons for all 3 channels

--- Assets/Scripts/VivoxManager.cs
+++ Assets/Scripts/VivoxManager.cs
@@ -6,17 +6,44 @@ using Random = UnityEngine.Random;
 
 public class VivoxManager : MonoBehaviour
 {
+    private ChannelOptions channelOptions;
     private async void Start()
     {
         await UnityServices.Instance.InitializeAsync();
         await VivoxService.Instance.InitializeAsync();
 
-        LoginOptions options = new();
-        options.DisplayName = $"Test Name {Random.Range(0, 100f)}{Random.Range(0,100f)}";
+        LoginOptions options = new()
+        {
+            DisplayName = $"Player {Guid.NewGuid()}"
+        };
         await VivoxService.Instance.LoginAsync(options);
+        
+        channelOptions = new()
+        {
+            MakeActiveChannelUponJoining = true
+        };
+    }
 
-        ChannelOptions channelOptions = new();
-        channelOptions.MakeActiveChannelUponJoining = true;
+    public async void JoinEchoChannel()
+    {
+        await VivoxService.Instance.LeaveAllChannelsAsync();
+        await VivoxService.Instance.JoinEchoChannelAsync("test", ChatCapability.AudioOnly, channelOptions);
+    }
+    
+    public async void JoinGroupChannel()
+    {
+        await VivoxService.Instance.LeaveAllChannelsAsync();
         await VivoxService.Instance.JoinGroupChannelAsync("test", ChatCapability.AudioOnly, channelOptions);
+    }
+    
+    public async void JoinPositionalChannel()
+    {
+        await VivoxService.Instance.LeaveAllChannelsAsync();
+
+        var channel3dProperties = new Channel3DProperties()
+        {
+
+        };
+        await VivoxService.Instance.JoinPositionalChannelAsync("test", ChatCapability.AudioOnly, channel3dProperties, channelOptions);
     }
 }
